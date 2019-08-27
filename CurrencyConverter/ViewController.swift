e774015a8bc6d599977b20b6c9db80844b12e1f2
Marcus Lewis <Marcuslewis7@icloud.com>
2019-08-27 21:14:48 +0100

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Marcus Lewis on 27/08/2019.
+//  Copyright © 2019 Marcus Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
