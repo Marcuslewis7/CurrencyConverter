81eef2c8801d2ee9b457bf71bba076dcee9ed156
Marcus Lewis <Marcuslewis7@icloud.com>
2019-08-30 23:29:24 +0100

    Added Unit tests for initial MVC Logic applied. Ensures that each Country's value from DataService is the same and has not been changed or tampered with. Also checks that the correct number of countries are present.

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -19,13 +19,97 @@ class CurrencyConverterTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCountryModel() {
+        //test evaulates if all items from countries in DataService are fed in.
+        let dataService = DataService.instance.getCountries().count
+        XCTAssertEqual(dataService, 4)
     }
-
+    
+    //Checks that initial for United Kingdom is still GBP
+    func testInitialGBP(){
+        let countryModel = DataService.instance.countries[0].initial
+        let chosenCountry = "GBP"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that initial for United States is still USD
+    func testInitialUSD(){
+        let countryModel = DataService.instance.countries[1].initial
+        let chosenCountry = "USA"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that initial for Australlia is still AUD
+    func testInitialAUD(){
+        let countryModel = DataService.instance.countries[2].initial
+        let chosenCountry = "AUD"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that initial for France is still FRA
+    func testInitialFRA(){
+        let countryModel = DataService.instance.countries[3].initial
+        let chosenCountry = "FRA"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that the Country Name of UK is still "United Kingdom"
+    func testCountryNameUK(){
+        let countryModel = DataService.instance.countries[0].countryName
+        let chosenCountry = "United Kingdom"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that the Country Name of USA is still "United States"
+    func testCountryNameUSA(){
+        let countryModel = DataService.instance.countries[1].countryName
+        let chosenCountry = "United States"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that the Country Name of Australia is still "Australia"
+    func testCountryNameAUD(){
+        let countryModel = DataService.instance.countries[2].countryName
+        let chosenCountry = "Australia"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks that the Country Name of France is still "France"
+    func testCountryNameFRA(){
+        let countryModel = DataService.instance.countries[3].countryName
+        let chosenCountry = "Australia"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks the name of the png file is still correct
+    func testCountryFlagUK(){
+        let countryModel = DataService.instance.countries[0].flag
+        let chosenCountry = "GBP.png"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks the name of the png file is still correct
+    func testCountryFlagUSA(){
+        let countryModel = DataService.instance.countries[1].flag
+        let chosenCountry = "USA.png"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks the name of the png file is still correct
+    func testCountryFlagAUD(){
+        let countryModel = DataService.instance.countries[2].flag
+        let chosenCountry = "AUD.png"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
+    //Checks the name of the png file is still correct
+    func testCountryFlagFRA(){
+        let countryModel = DataService.instance.countries[3].flag
+        let chosenCountry = "FRA.png"
+        XCTAssertEqual(countryModel, chosenCountry)
+    }
+    
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
